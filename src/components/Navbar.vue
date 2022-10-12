39a8448e8c84ef052c02819d0bc503c9b03cb0bc
FaridIzquierdo <u202015215@upc.edu.pe>
2022-10-12 14:19:59 -0500

    feature/nabvarView
    :)

---
+++ src/components/Navbar.vue
@@ -0,0 +1,15 @@
+<template>
+  <header>
+
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Navbar"
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import router from './router'
 
 import './assets/main.css'
 
+
 const app = createApp(App)
 
 app.use(createPinia())
