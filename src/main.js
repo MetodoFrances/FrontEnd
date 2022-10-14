a0f98ab0244d13cc684bf57bf78445084ecc9bd3
Miguel445Ar <86012555+Miguel445Ar@users.noreply.github.com>
2022-10-13 21:02:58 -0500

    added primevue and prime icons dependecies

--- src/main.js
+++ src/main.js
@@ -1,16 +1,13 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
 import App from './App.vue'
-import router from './router'
-
-import './assets/main.css'
+import PrimeVue from 'primevue/config';
+import 'primeicons/primeicons.css';
 
 const app = createApp(App)
 
 // Develop content
-
+app.use(PrimeVue);
 app.use(createPinia())
-app.use(router)
 
 app.mount('#app')
