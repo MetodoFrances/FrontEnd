652fd303d3a4c35d975228eb482f3cdb75a4b103
Miguel445Ar <86012555+Miguel445Ar@users.noreply.github.com>
2022-10-13 21:30:58 -0500

    added base project folder structure

---
+++ src/featureName/pages/feature-name.component.vue
@@ -0,0 +1,27 @@
+<template>
+  <h1>{{ msg }}</h1>
+</template>
+
+<script>
+// Pueden importar otros componentes aquí
+import FeatureNameService from "../services/feature-name.service.js"; // Importando el archivo .js
+export default {
+    name: "FeatureNameComponent",
+    data() {
+        return {
+            msg: "This is an sample component"
+        }
+    },
+    created() {
+        service = new FeatureNameService()
+        console.log(service.doSomeStuff())
+    }
+}
+</script>
+
+<style scoped>
+/*
+    - con scoped => los estilos solo afectan a este componente
+    - sin escoped => los estilos afectarán a los componentes que este componente contega
+*/
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,14 @@
+import { createRouter, createWebHistory } from "vue-router";
+const routes = [
+];
+
+const router = createRouter({
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes,
+    scrollBehavior(to, from, savedPosition) {
+      if (savedPosition) return savedPosition;
+      else return { left: 0, top: 0 };
+    },
+});
+
+export default router;
