5ccfedd15399078576bd045e2a7aed5979ea5b01
Miguel445Ar <86012555+Miguel445Ar@users.noreply.github.com>
2022-10-21 10:12:03 -0500

    feat: added new style classes to improve layout appearance

--- src/payment-shedule/pages/payment-schedule.component.vue
+++ src/payment-shedule/pages/payment-schedule.component.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="ps-container">
-    <div class="inputsContainer p-input-filled">
+    <div class="inputsContainer p-input-filled first">
         <pv-input-text id="salePrice" type="number" v-model="loanDetails.salePrice" placeholder="Sale price"></pv-input-text>
         <pv-input-text class="inputText" type="number" v-model="loanDetails.years" placeholder="Years" min="1"></pv-input-text>
         <pv-input-text type="number" v-model="loanDetails.paymentFrecuencyInDays" placeholder="Payment frecuency in days" min="1"></pv-input-text>
@@ -10,16 +10,22 @@
         <pv-input-text type="number" v-model="loanDetails.incomeTaxPercentage" placeholder="Income Tax Percentage" readonly></pv-input-text>
         <pv-input-text type="number" v-model="loanDetails.buyBackPercentage" placeholder="Buyback percentage" min="0.0001" step="0.0001"></pv-input-text>
     </div>
-    <div class="inputsContainer p-input-filled">
+    <div class="inputsContainer p-input-filled second">
         <pv-input-text type="number" v-model="initialCosts.notarialCosts" placeholder="Notarial costs"></pv-input-text>
         <pv-input-text type="number" v-model="initialCosts.registrationCosts" placeholder="Registration costs" min="1" step="0.05"></pv-input-text>
         <pv-input-text type="number" v-model="initialCosts.appraisal" placeholder="Appraisal" min="1" step="0.05"></pv-input-text>
         <pv-input-text type="number" v-model="initialCosts.studyCommission" placeholder="Study commission" min="1" step="0.05"></pv-input-text>
     </div>
+    <div class="inputsContainer p-input-filled third">
+        <pv-input-text type="number" v-model="periodicCosts.periodicCommission" placeholder="Periodic Commission" min="0.05" step="0.05"></pv-input-text>
+        <pv-input-text type="number" v-model="periodicCosts.riskInsurancePercentage" placeHolder="Risk insurance percentage" min="0.001" step="0.001"></pv-input-text>
+    </div>
   </div>
-  <pv-button label="Get Payment Schedule" icon="pi pi-table" class="p-button-raised p-button-rounded p-button-warning" @click="getPaymentSchedule"/>
-  <pv-button label="Get Leasing Results" icon="pi pi-caret-right" class="p-button-raised p-button-rounded p-button-warning"/>
-  <pv-button label="Save Leasing" icon="pi pi-save" class="p-button-raised p-button-rounded p-button-warning" @click="saveLeasing"/>
+  <div class="ps-buttons">
+    <pv-button label="Get Payment Schedule" icon="pi pi-table" class="p-button-raised p-button-rounded p-button-warning" @click="getPaymentSchedule"/>
+    <pv-button label="Get Leasing Results" icon="pi pi-caret-right" class="p-button-raised p-button-rounded p-button-warning"/>
+    <pv-button label="Save Leasing" icon="pi pi-save" class="p-button-raised p-button-rounded p-button-warning" @click="saveLeasing"/>
+  </div>
 </template>
 
 <script>
@@ -37,9 +43,9 @@ export default {
     data() {
         return {
             salePrice: null,
-            initialCosts: new InitialCosts(250.0, 150.0, 80.0, 100.5, 50.0),
-            periodicCosts: new PeriodicCosts(10.0, 0.003),
-            loanDetails: new LoanDetails(11800, 3, 30, 0.12, 0.01, 0.25),
+            initialCosts: new InitialCosts(null, null, null, null, null),
+            periodicCosts: new PeriodicCosts(null, null),
+            loanDetails: new LoanDetails(null, null, null, null, null, null),
             opportunityCosts: new OpportunityCosts(),
             graceType: "None",
             graceTypePeriods: 0
@@ -58,19 +64,46 @@ export default {
 
 <style>
 .inputsContainer {
-    width: 30rem;
-    height: 30rem;
+    width: 100%;
+    height: 100%;
     display: flex;
     flex-direction: column;
-    justify-content: space-between;
+    justify-content: space-evenly;
+    padding: 0 1.5rem;
+    border-radius: 10px;
 }
 .ps-container {
     width: 100%;
     display: grid;
-    grid-template-columns: 50% 50%;
+    grid-template-columns: repeat(2,45%);
+    grid-template-rows: 25rem 25rem;
+    grid-template-areas:
+    "first second"
+    "first third";
+    column-gap: 10%;
+    row-gap: 5rem;
     align-content: space-between;
+    padding: 2rem;
+}
+.ps-buttons {
+    width: 100%;
+    display: flex;
+    justify-content: space-evenly;
+    margin: 2.5rem 0;
 }
 .btn {
     color: #FFF;
+}
+.first {
+    grid-area: first;
+    background-color: rgb(221, 52, 52);
+}
+.second {
+    grid-area: second;
+    background-color: rgb(34, 152, 207);
+}
+.third {
+    grid-area: third;
+    background-color: rgb(69, 236, 63);
 }
 </style>
