022883310fd90dba3b3d7616bfe72c1b54012adc
Miguel445Ar <86012555+Miguel445Ar@users.noreply.github.com>
2022-11-04 22:49:53 -0500

    fix: erased unnecessary folders

--- src/shared/services/http-common.js
+++ src/shared/services/http-common.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:3000/api/v1",
+  baseURL: "http://localhost:3000",
   headers: { "Accept": "application/json", "Content-Type": "application/json" },
 });
