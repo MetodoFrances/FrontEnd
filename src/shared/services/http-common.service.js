8367b628ed5e7135ddac5aa28aead2656ba5e778
Miguel445Ar <86012555+Miguel445Ar@users.noreply.github.com>
2022-11-23 10:23:36 -0500

    feat: changed axios base url

--- src/shared/services/http-common.service.js
+++ src/shared/services/http-common.service.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL : "http://localhost:8090",
+    baseURL : "https://leasingfy-back.herokuapp.com",
     headers : { "Accept": "application/json", "ContentType" : "application/json" }
 });
