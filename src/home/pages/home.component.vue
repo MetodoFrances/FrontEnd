4a9d6aa17157d2961fc4f81a9766c3f2ae0180c6
FaridIzquierdo <u202015215@upc.edu.pe>
2022-11-23 18:01:59 -0500

    update

--- src/home/pages/home.component.vue
+++ src/home/pages/home.component.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="pdf-container">
-        <a title="Logo Leasing" href="https://docs.google.com/document/d/1gus9P2s-ZNI9y_hy1V8WhF2NZjc6Jh_MF-aTbD7HczA/edit" target="_blank"><img src="../../../src/assets/logoleasing.webp" alt="Logo" /></a>
+        <a title="Logo Leasing" href="https://docs.google.com/document/d/1gus9P2s-ZNI9y_hy1V8WhF2NZjc6Jh_MF-aTbD7HczA/edit" target="_blank"><img class="logo" src="../../../src/assets/logoleasing.webp" alt="Logo" /></a>
     </div>
 </template>
 
@@ -41,5 +41,8 @@ export default {
     width: 100%;
     height: 80rem;
 }
-
+.logo{
+  height: 1000px;
+  width: 1200px;
+}
 </style>
